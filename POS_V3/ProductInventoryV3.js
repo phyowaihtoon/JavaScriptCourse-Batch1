eae81e2cd955c3314f552ab9355c693b2bdbb6b9
phyowaihtoon <phyogyi.java@gmail.com>
2026-03-05 17:00:36 +0800

    Add eidit button

--- POS_V3/ProductInventoryV3.js
+++ POS_V3/ProductInventoryV3.js
@@ -2,9 +2,9 @@ let productId = 1;
 
 let products = []; //length = 0
 
-
 document.getElementById("btn-add").addEventListener("click", addProduct);
 document.getElementById("btn-clear").addEventListener("click", clearProduct);
+document.getElementById("btn-update").addEventListener("click", updateProduct);
 
 
 function addProduct() {
@@ -42,7 +42,7 @@ function addProduct() {
 
 
     let validationResult = validateProduct(product); // {}
-    
+
 
     if (!validationResult.valid) {
         alert(validationResult.message);
@@ -54,6 +54,8 @@ function addProduct() {
     productId++;
 
     showProducts(); //function call to show products in table
+
+    clearProduct();
 }
 
 function validateProduct(product) {
@@ -66,7 +68,7 @@ function validateProduct(product) {
     if (product.code === "") {
         result.valid = false;
         result.message = "Product Code is required";
-        
+
         return result;
     }
 
@@ -116,11 +118,13 @@ function showProducts() {
         let row = `<tr>
                     <td>${product.id}</td>
                     <td>${product.code}</td>
+                    <td>${product.barcode}</td>
                     <td>${product.name}</td>
+                    <td>${product.costPrice}</td>
                     <td>${product.sellingPrice}</td>
                     <td>${product.quantity} </td>
                     <td class="action-column">
-                        <button class="btn-edit">Edit</button>
+                        <button class="btn-edit" onclick="editProduct(${product.id})">Edit</button>
                         <button class="btn-delete" onclick="deleteProduct(${product.id})">Delete</button>
                     </td>
                 </tr>`;
@@ -132,30 +136,106 @@ function showProducts() {
 
 //Implement a function to delete product from products array and show in table
 function deleteProduct(productId) {
-    
+
     let index = -1; // to store index of an array
 
     //Find the index of search product 
-    for(let i=0; i<products.length; i++){
+    for (let i = 0; i < products.length; i++) {
         let productObj = products[i];
 
-        if(productObj.id === productId){
+        if (productObj.id === productId) {
             index = i;
             break;
         }
     }
 
     //Shift Elements
-    for(let i=index; i < products.length -1; i++){
-        products[i] = products[i+1];
+    for (let i = index; i < products.length - 1; i++) {
+        products[i] = products[i + 1];
     }
 
-    products.length --;
+    products.length--;
 
     //Show Products
     showProducts();
 }
 
+function editProduct(productId) {
+
+    //1. Find product to edit , and show data in UI
+    for (let i = 0; i < products.length; i++) {
+        let productObj = products[i]; //Retrieve product object from array
+
+        if (productObj.id === productId) {
+            //Show data in UI
+            document.getElementById("product-id").value = productObj.id;
+            document.getElementById("product-code").value = productObj.code;
+            document.getElementById("product-barcode").value = productObj.barcode;
+            document.getElementById("product-name").value = productObj.name;
+            document.getElementById("category").value = productObj.categoryId;
+            document.getElementById("supplier").value = productObj.supplier;
+            document.getElementById("expiry-date").value = productObj.expiryDate;
+            document.getElementById("status").selectedIndex = productObj.status;
+            document.getElementById("cost-price").value = productObj.costPrice;
+            document.getElementById("selling-price").value = productObj.sellingPrice;
+            document.getElementById("quantity").value = productObj.quantity;
+            document.getElementById("unit-of-measure").value = productObj.unitOfMeasure;
+
+            break;
+        }
+    }
+
+
+}
+
+
+function updateProduct() {
+    let updatedProductID = document.getElementById("product-id").value;
+    let productCode = document.getElementById("product-code").value;
+    let barcode = document.getElementById("product-barcode").value;
+    let productName = document.getElementById("product-name").value;
+
+    let categoryId = document.getElementById("category").value; // 1,2,3,4,5,6
+    let supplier = document.getElementById("supplier").value;
+    let expiryDate = document.getElementById("expiry-date").value;
+    let status = document.getElementById("status").value;
+
+    let costPrice = document.getElementById("cost-price").value;
+    let sellingPrice = document.getElementById("selling-price").value;
+
+    let quantity = document.getElementById("quantity").value;
+    let unitOfMeasure = document.getElementById("unit-of-measure").value;
+
+
+    //Add variables to product object
+    let product = {
+        id: updatedProductID,
+        code: productCode,
+        barcode: barcode,
+        name: productName,
+        categoryId: categoryId,
+        supplier: supplier,
+        expiryDate: expiryDate,
+        status: status,
+        costPrice: costPrice,
+        sellingPrice: sellingPrice,
+        quantity: quantity,
+        unitOfMeasure: unitOfMeasure
+    };
+
+    //4. Update into array
+    for (let i = 0; i < products.length; i++) {
+        let productObj = products[i];
+
+        if (productObj.id === Number(product.id)) {
+            products[i] = product;
+            break;
+        }
+    }
+
+
+    showProducts();
+}
 
 function clearProduct() {
     document.getElementById("product-id").value = "";
